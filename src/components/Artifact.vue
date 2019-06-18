466ee8324b77a0192ab0d6c6b63ef354c5975ad1
tocomi <tocomi@wish.ocn.ne.jp>
2019-06-19 01:59:25 +0900

    tweek artifact style

--- src/components/Artifact.vue
+++ src/components/Artifact.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
   div.artifact
-    a.title(:href="artifact.link" target="_blank") {{ artifact.title }}
-    p {{ artifact.description }}
-    a(:href="artifact.github" target="_blank")
-      img.icon(alt="github" src="../assets/github.png")
+    div.header
+      a.title(:href="artifact.link" target="_blank") {{ artifact.title }}
+    div.content
+      p.description {{ artifact.description }}
+      a(:href="artifact.github" target="_blank")
+        img.icon(alt="github" src="../assets/github.png")
 </template>
 
 <script lang="ts">
@@ -22,14 +24,26 @@
 
 <style lang="scss" scoped>
 .artifact {
-  .title {
-    font-size: 24px;
-    font-weight: bold;
-    text-decoration: none;
+  border-radius: 5px;
+  .header {
+    padding: 10px;
+    border-radius: 10px 10px 0 0;
+    background-color: #EA8A2A;
+    .title {
+      font-size: 20px;
+      font-weight: bold;
+      text-decoration: none;
+      color: #FFFFFF;
+    }
   }
-  .icon {
-    height: 50px;
-    width: 50px;
+  .content {
+    padding: 10px;
+    border-radius: 0 0 10px 10px;
+    background-color: #EAEAEA;
+    .icon {
+      height: 50px;
+      width: 50px;
+    }
   }
 }
 </style>
