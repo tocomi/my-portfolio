27038811d5adf6c1cd6387a1d507a5f60178c71d
tocomi <tocomi@wish.ocn.ne.jp>
2019-06-25 01:18:09 +0900

    update introduction

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,16 @@
     h2 About
     img#icon(alt="Vue logo" src="../assets/2896.jpg")
     div#introduction
-      h3#name @tocomi
-      div#text
-        p 社会人になるのをきっかけにエンジニアになりました。
-        p 個人でWebアプリケーションを開発するのが趣味です。
+      div
+        h3#name @tocomi
+        p Webエンジニアを志しています。
+      div.section
         h3 経歴
         p.bold 2010.04- 東北大学経済学部
         p.bold 2015.04- ワークスアプリケーションズ
         p - Webアプリケーションのバグ修正(Java,html,js,css)
         p - 社内ツールの開発・マネジメント(Java,AWS)
+      div.section
         h3 Skill
         p Java Ruby Rails Python JavaScript Vue.js ReactNative jQuery html pug css sass Delphi COBOL shell
         p EC2 cloudformation lambda firebase Jenkins Ansible Docker Oracle PostgreSQL
@@ -37,6 +38,9 @@ export default class Home extends Vue {}
   #introduction {
     .bold {
       font-weight: bold;
+    }
+    .section {
+      margin: 50px 0 0 0;
     }
   }
 }
